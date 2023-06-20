71258ae99b5fe5774a3e31edbf7a36e0c55f4cec
Leila Moraes <leila.violino@gmail.com>
2023-06-20 18:52:42 -0300

    tests: implements test_ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("farinha")
+    assert ingredient.name == "farinha"
+    assert ingredient.restrictions == {Restriction.GLUTEN}
+    assert repr(ingredient) == "Ingredient('farinha')"
+
+    ingredient1 = Ingredient("salmão")
+    ingredient2 = Ingredient("salmão")
+    ingredient3 = Ingredient("presunto")
+    
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
